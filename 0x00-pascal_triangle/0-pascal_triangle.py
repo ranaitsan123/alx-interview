487ddbd12fb19e6d21260e46dfe2e3ece8cc1c12
ranaitsan123 <95276621+ranaitsan123@users.noreply.github.com>
2024-10-06 20:37:20 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,15 +4,18 @@ def pascal_triangle(n):
 
     Args:
         n (int): The number of rows in the triangle.
+
+    Returns:
+        list: A list of lists containing the values of Pascal's triangle.
     """
     if n <= 0:
         return []
-    
+
     triangle = []
     for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
+        row = [1] * (i + 1)  # Initialize the row with 1s
+        for j in range(1, i):  # Calculate the values for the current row
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    
+
     return triangle
