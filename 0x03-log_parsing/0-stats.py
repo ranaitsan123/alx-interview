bb6fa99a6db93a351dfc77689774b4e0477403a5
AICHA LAHNITE <halaicha300@gmail.com>
2024-10-27 22:46:10 +0100

    ch

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python3
-
+""" doc """
 import sys
 
-def print_msg(dict_sc, total_file_size):
-    """Print the status codes and total file size."""
-    print("File size: {}".format(total_file_size))
-    for key in sorted(dict_sc.keys()):
-        if dict_sc[key] > 0:
-            print("{}: {}".format(key, dict_sc[key]))
 
-total_file_size = 0
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+if __name__ == "__main__":
+    i = 0
+    status = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    fileSize = 0
 
-try:
-    for line in sys.stdin:
-        parsed_line = line.split()
-        
-        # Expect at least 2 elements: file size and status code
-        if len(parsed_line) < 2:
-            continue
+    def printstats(fileSize, status):
+        """ doc """
+        print("File size: {:d}".format(fileSize))
+        for key in sorted(status.keys()):
+            if status[key] != 0:
+                print("{}: {:d}".format(key, status[key]))
 
-        try:
-            file_size = int(parsed_line[0])  # First element is file size
-            status_code = parsed_line[1]      # Second element is status code
-        except ValueError:
-            continue  # Skip lines where conversion fails
-
-        total_file_size += file_size
-        if status_code in dict_sc:
-            dict_sc[status_code] += 1
-
-finally:
-    print_msg(dict_sc, total_file_size)
+    try:
+        for line in sys.stdin:
+            words = line.split()
+            if len(words) >= 2:
+                if words[-2] in status.keys():
+                    status[words[-2]] += 1
+                fileSize += int(words[-1])
+                i += 1
+                if not i % 10:
+                    printstats(fileSize, status)
+        printstats(fileSize, status)
+    except KeyboardInterrupt:
+        printstats(fileSize, status)
+        raise
